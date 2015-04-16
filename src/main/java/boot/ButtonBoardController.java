b83013d7f5ef948c5e15add20239b2a75a597272
Martin Sommervold <martin@sommervold.com>
2015-04-16 17:38:12 +0200

    lagt til bootstrap, litt ekstra css, samt ny controller "ButtonBoardController" for ruter som ikke har med REST-api å gjøre

---
+++ src/main/java/boot/ButtonBoardController.java
@@ -0,0 +1,20 @@
+package boot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+
+
+@Controller
+public class ButtonBoardController {
+    @RequestMapping(value = "/", method=RequestMethod.GET)
+    public String displayButtons(){
+
+        return "buttons";
+    }
+}
--- src/main/java/boot/PersonController.java
+++ src/main/java/boot/PersonController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 
+
 @Controller
 public class PersonController {
 	
