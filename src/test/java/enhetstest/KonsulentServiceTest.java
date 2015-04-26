1b2afaa592c940a7188e651eabe5486656958382
olovholm <ola.lovholm@soprasteria.com>
2015-04-26 21:39:54 +0200

    Legger til struktur for enhetstester samt to av disse

--- src/test/java/enhetstest/KonsulentServiceTest.java
+++ src/test/java/enhetstest/KonsulentServiceTest.java
@@ -2,20 +2,45 @@ package enhetstest;
 
 import com.soprasteria.boot.domain.Konsulent;
 import com.soprasteria.boot.service.KonsulentService;
-import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.Before;
+import org.junit.Assert;
 
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import java.util.List;
+
 
 /**
  * Created by olovholm on 21.04.2015.
  */
-
 public class KonsulentServiceTest {
+    List<Konsulent> konsulentList;
+    KonsulentService konsulentService;
+
+    @Before
+    public void setup() {
+        this.konsulentService = new KonsulentService();
+        this.konsulentList = konsulentService.hentAlleKonsulenter();
+    }
 
 
+    @Test
+    public void addKonsulentIncreasesSizeOnList() {
+        Konsulent k = new Konsulent();
+        int initalSize = this.konsulentList.size();
+        this.konsulentList.add(k);
+        Assert.assertEquals(initalSize+1, this.konsulentList.size());
+    }
+
+    @Test
+    public void hentMedIdFetchesCorrectObject() {
+        Konsulent k = new Konsulent();
+        k.setNavn("TestTestTest");
+        k.setId(101);
+        this.konsulentList.add(k);
+        Konsulent konsulent_kopi = this.konsulentService.hentMedId(101);
+        Assert.assertEquals(k, konsulent_kopi);
+
+    }
 
 
 }
